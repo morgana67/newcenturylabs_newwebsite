94b39162e2e9bd908ad7e25a859067a6820394e6
luong.nguyen.dai <MY_NAME@example.com>
2021-01-27 22:18:42 +0700

    Edit menu

--- public/front/js/main.js
+++ public/front/js/main.js
@@ -3,4 +3,21 @@ $(function () {
         e.preventDefault();
         $('.logout-form').submit();
     })
+
+    $('.navbar-toggle').on('click',function(){
+        if($(this).hasClass('show_menu')){
+            $(this).removeClass('show_menu');
+            $(this).addClass('collapsed');
+            $('#slidemenu').removeClass('menu_mobile');
+            $('.navbar-header').css('transform','none');
+            $('main#page-content').css('transform','none');
+
+        }else{
+            $(this).removeClass('collapsed');
+            $(this).addClass('show_menu');
+            $('#slidemenu').addClass('menu_mobile');
+            $('.navbar-header').css('transform','translateX(-245px)');
+            $('main#page-content').css('transform','translateX(-245px)');
+        }
+    })
 })
