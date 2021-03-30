c2a4294a65c71178bce5657b95c3f515afaceaf8
luong.nguyen.dai <luong.nguyen.dai@yumeshin-vn.com>
2021-03-30 09:16:32 +0700

    Fix issue

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -113,7 +113,10 @@ class ProfileController extends Controller
     }
 
     public function orderDetail(Request $request,$id){
-        $order = Order::where('id',$id)->where('customer_id',user()->getAuthIdentifier())->with('details','customer','country')->first();
+        $order = Order::where('id',$id)->where(function ($query){
+            $query->where('customer_id','=',user()->getAuthIdentifier());
+            $query->orWhere('email','=',user()->email);
+        })->with('details','customer','country')->first();
         if(empty($order)) return abort(404,'You do not have permission to view this Order');
         return view('profile.order-detail',compact('order'));
     }
