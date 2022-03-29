dffdba04c3041f97468c49dd84af600bc05cad5b
NamMT <nam.mactung@powergatesoftware.com>
2022-03-29 09:42:19 +0700

    commit Tag

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -25,6 +25,19 @@
 
     <link rel="stylesheet" href="{{asset('front/css/app.css')}}">
     @yield('css')
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5XR2WRM');</script>
+<!-- End Google Tag Manager -->
+Additionally, paste this code immediately after the opening <body> tag:
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XR2WRM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 </head>
 <body class="transition nav-plusminus slide-navbar slide-navbar--left">
     @include('layouts.header')
