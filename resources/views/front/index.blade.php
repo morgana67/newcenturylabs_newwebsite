524e8ba9738af6507b1c652c333a39d398de9bb0
nam.mac.tung <mactungnam96@gmail.com>
2022-03-31 22:55:02 +0700

    Commit New Feature Home Page

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -185,7 +185,7 @@
             </div>
             @for($i = 1; $i <= 8; $i++)
                 @if ($i == 1 || $i == 5)
-                    <div class="our-services">
+                    <div class="our-services" style="margin-top: 2rem">
                 @endif
                 @php $imageName = "image_{$i}_section5" @endphp
                     <div><img src="{{ isset($body->$imageName) ? image($body->$imageName) : "https://via.placeholder.com/200" }}" alt=""></div>
