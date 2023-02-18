69ca1bb7953f11c371226ebe08601c2fe9bd9e73
Keith Jordan <keith.jordan@healthcareintegrations.com>
2023-02-17 20:36:05 -0500

    pwn calls disabled 1

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -202,7 +202,7 @@ class CheckoutController extends Controller
 
     public function curl($field = array()){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,env('PWN_END_POINT_ORDER'));
+        // curl_setopt($ch, CURLOPT_URL,env('PWN_END_POINT_ORDER'));
         if ($field && !empty($field)) {
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $field);
